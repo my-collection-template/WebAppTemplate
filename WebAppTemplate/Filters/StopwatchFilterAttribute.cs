6835ccad5f3c9f20cb66ce960f81aa10f49b1ee1
HakamFostok <hakam-2009@windowslive.com>
2017-08-03 13:49:01 +0300

    Add project files.

---
+++ WebAppTemplate/Filters/StopwatchFilterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Diagnostics;
+
+namespace WebAppTemplate.Filters
+{
+    public class StopwatchFilterAttribute : ActionFilterAttribute
+    {
+        private const string StopwatchKey = "StopwatchFilter.Value";
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            actionContext.Request.Properties[StopwatchKey] = Stopwatch.StartNew();
+            base.OnActionExecuting(actionContext);
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            Stopwatch stopwatch = (Stopwatch)actionExecutedContext.Request.Properties[StopwatchKey];
+            if (actionExecutedContext.Response != null)
+                actionExecutedContext.Response.Headers.Add("X-Runtime", stopwatch.Elapsed.TotalMilliseconds.ToString());
+            base.OnActionExecuted(actionExecutedContext);
+        }
+
+    }
+}
---
+++ WebAppTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTemplate.Startup))]
+namespace WebAppTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
