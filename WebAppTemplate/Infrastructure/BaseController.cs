db7a6ef7f44d85a1493611443265b299a41a75a8
HakamFostok <hakam-2009@windowslive.com>
2017-08-08 11:19:06 +0300

    - Add the Render of the view as string ability to the BaseController

---
+++ WebAppTemplate/Infrastructure/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTemplate.Infrastructure
+{
+    public class BaseController : Controller
+    {
+        protected string RenderRazorViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+
+                ViewContext viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
